4690a085cd5cef8a53f3aba841648c9c1ee32ad4
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-05-16 11:28:36 +0200

    Finished post crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { PostsModule } from './posts/posts.module';
+import { CategoriesModule } from './categories/categories.module';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    PostsModule
+    PostsModule,
+    CategoriesModule
   ]
 })
 export class AppModule {}
