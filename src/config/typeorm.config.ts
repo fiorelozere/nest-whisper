d75049703a761f0bd96336cd2c57f892b52641f5
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-05-31 21:01:17 +0200

    Implemented config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,16 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as config from 'config'
 
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModule = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Fiorelo03',
-  database: 'nest-whisper',
+  type: dbConfig.type,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
   entities: ["dist/**/*.entity{.ts,.js}"],
-  synchronize: true,
+  synchronize: dbConfig.synchronize,
 
 }
