2da9fd48e2058f3a3d7aac84007ae131c1c33130
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-06-08 15:26:58 +0200

    configuring env variables

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './jwt.strategy';
   imports: [
     PassportModule.register({defaultStrategy: 'jwt'}),
     JwtModule.register({
-      secret: 'Secretsecret',
+      secret: process.env.JWTSECRET || 'Secretsecret',
       signOptions: {
         expiresIn: 3600000
       }
