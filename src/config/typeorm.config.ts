ae3297631f33377c850f41635e477bf760a07709
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-06-08 14:31:52 +0200

    configuring db

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,11 +6,7 @@ const dbConfig = config.get('db');
 export const typeOrmConfig: TypeOrmModule = {
   url: process.env.DATABASE_URL,
   type: dbConfig.type,
-  host: dbConfig.host,
   port: dbConfig.port,
-  username: dbConfig.username,
-  password: dbConfig.password,
-  database: dbConfig.database,
   entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: dbConfig.synchronize,
 
