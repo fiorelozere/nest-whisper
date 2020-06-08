a1c42c087213976c1429ff4943336ca629e65426
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-06-08 14:47:36 +0200

    adding dist

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,6 +7,7 @@ export const typeOrmConfig: TypeOrmModule = {
   url: process.env.DATABASE_URL,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
+  host: process.env.HOST,
   type: dbConfig.type,
   port: dbConfig.port,
   entities: ["dist/**/*.entity{.ts,.js}"],
