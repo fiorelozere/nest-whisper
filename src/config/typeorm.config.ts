226f94a99cbf84a446b6878260b00a44cfd9719b
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-06-08 14:45:42 +0200

    adding dist

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,7 +4,9 @@ import * as config from 'config'
 const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModule = {
-  url: "postgres://cztchxjkhblssf:05a4deb2d624c7c5b5b85b2b7b9615154cedd5489d25d1f94c3bef6ed4c9cc2d@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d95367h4ee1q2n",
+  url: process.env.DATABASE_URL,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
   type: dbConfig.type,
   port: dbConfig.port,
   entities: ["dist/**/*.entity{.ts,.js}"],
