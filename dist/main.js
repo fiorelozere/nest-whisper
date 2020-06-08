987c3edd93b76caffe22a168544b3bee8c793b29
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-06-08 15:44:26 +0200

    helmet implemented

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const config = require("config");
+const helmet = require("helmet");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const serverConfig = config.get('server');
+    app.use(helmet());
     await app.listen(process.env.PORT || serverConfig.port);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config'
-
+import * as helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const serverConfig = config.get('server') ;
-
+  app.use(helmet());
   await app.listen(process.env.PORT || serverConfig.port);
+
 }
 bootstrap();
